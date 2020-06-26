986ce4322c6786fb18c422d64dfbfcc380e76958
Aref <storm.aref@gmail.com>
2020-06-26 10:15:13 +0430

    backup

---
+++ Factor/ConfirmCodeView.swift
@@ -0,0 +1,21 @@
+//
+//  ConfirmCodeView.swift
+//  Factor
+//
+//  Created by Aref Azizian on 6/26/20.
+//  Copyright © 2020 Aref Azizian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ConfirmCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfirmCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmCodeView()
+    }
+}
--- Factor/ContentView.swift
+++ Factor/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Alamofire
 
 func GetFont(size: CGFloat) -> Font {
     return Font.custom("Shabnam-Light", size: CGFloat(size))
@@ -40,8 +41,10 @@ func hexStringToUIColor (hex:String) -> UIColor {
     )
 }
 
+
 struct ContentView: View {
     @State private var phone : String = ""
+    @State private var isActiove : Bool = false
     
     let msg = "شماره همراه خود را برای دریافت کد فعالسازی وارد کنید"
     
@@ -54,42 +57,72 @@ struct ContentView: View {
         UITableView.appearance().separatorStyle = .none
     }
     
-    var body: some View {
-        VStack{
-            
-            VStack(alignment: .trailing){
-                Text("سلام !").font(GetFont(size: 32)).multilineTextAlignment(.trailing).frame(width: 100, height: 52, alignment: .trailing)
+    func Login(Phone:String) -> Void {
+        let url = "http://app.bazarsefid.com/api/Login/Login?phone=" + Phone
+        let request = AF.request(URL.init(string: url)!,method: .post ,encoding: JSONEncoding.default)
+        request.responseString{
+            (response) in
+            switch response.result {
                 
-                Text(msg).font(GetFont(size: 14)).multilineTextAlignment(.trailing).frame(width: 175, height: 46, alignment: .trailing).foregroundColor(grey)
-                
-                
-            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 80, alignment: .trailing).padding()
-            
-            Form{
-                Section{
-                    TextField("شماره تلفن",text:$phone).font(GetFont(size: 16)).multilineTextAlignment(.trailing).foregroundColor(grey)
+            case .success(_):
+                let result = response.value
+                print(result ?? "null result")
+                let code = response.response?.statusCode
+                if code==200{
+                    self.isActiove = true
                 }
-                
-                Section{
-                    Button(action: {
-                        
-                    }){
-                        Text("ارسال کد فعال سازی").font(GetFont(size: 14)).multilineTextAlignment(.center)
-                    }
-                    .frame(width:144, height:36, alignment: .center)
-                    .background(purple).foregroundColor(Color.white).multilineTextAlignment(.trailing)
-                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .trailing)
-                    
-                    .disabled(!self.phone.matches("^(0)?9([0-9]{9})$"))
-            }.background(Color.white)
-            
-            Image("objects").resizable().aspectRatio(contentMode: .fit)
+                break
+            case .failure(let error):
+                let des = error.errorDescription
+                let code = error.responseCode
+                print(des ?? "null des",code ?? 0)
+                break
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                
+                    NavigationLink(destination: ConfirmCodeView(),isActive: $isActiove){
+                        Text("")
+                    }
+                
+                    VStack(alignment: .trailing){
+                        Text("سلام !").font(GetFont(size: 32)).multilineTextAlignment(.center).padding(11.0).frame(width: 110, height: 60, alignment: .trailing)
+                        
+                        Text(msg).font(GetFont(size: 14)).multilineTextAlignment(.trailing).frame(width: 175, height: 46, alignment: .trailing).foregroundColor(grey)
+                        
+                        
+                    }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 120, alignment: .trailing)
+                    
+                    Form{
+                        Section{
+                            TextField("شماره تلفن",text:$phone).font(GetFont(size: 16)).multilineTextAlignment(.trailing).foregroundColor(grey).keyboardType(.numberPad)
+                        }
+                        
+                        Section{
+                            Button(action: {
+                                self.Login(Phone: self.phone)
+                            }){
+                                Text("ارسال کد فعال سازی").font(GetFont(size: 14)).multilineTextAlignment(.center)
+                            }
+                            .frame(width:144, height:36, alignment: .center)
+                            .background(purple).foregroundColor(Color.white).multilineTextAlignment(.trailing)
+                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .trailing)
+                            
+                            .disabled(!self.phone.matches("^(0)?9([0-9]{9})$"))
+                    }.background(Color.white)
+                    
+                    Image("objects").resizable().aspectRatio(contentMode: .fit)
+                }.padding()
+            }
+        }
     }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
 }
